8d506281f07b2a626ae5537334ba041471e154af
Gudala Nandini <gudalanandini2939@gmail.com>
2026-06-06 21:04:32 +0530

    Fix CORS for deployed frontend

--- server/server.js
+++ server/server.js
@@ -14,7 +14,16 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://task-flow-mern-one.vercel.app",
+      "https://task-flow-mern-42s5nxbmz-gudalanandini.vercel.app"
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
